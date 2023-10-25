fa7e93c164d125a1e4f2c3481b525e7a3a900c22
Hiuconymoi <hieunvgch211369@fpt.edu.vn>
2023-10-25 21:18:44 +0700

    update order

--- routes/cart.js
+++ routes/cart.js
@@ -6,42 +6,106 @@ const UserModel=require('../models/UserModel');
 const PrincessModel=require('../models/PrincessModel');
 
 
-router.get('/',async(req,res)=>{
-    var userId=req.session.userId;
-    try{
-        var cart=await CartModel.findOne({userId:userId});
+// router.get('/',async(req,res)=>{
+//     var userId=req.session.userId;
+//     try{
+//         var cart=await CartModel.findOne({userId:userId});
+//         if(!cart){
+//             return res.status(404).json({message:"Cart not found"})
+//         }
+//         var items = cart.items;
+// var productOrder = [];
+// var total = 0.0;
+
+// for (var i = 0; i < items.length; i++) {
+//     var product = items[i].product;
+//     var quantity = items[i].quantity;
+
+//     // Now you can use product to find the product information
+//     var productInfor = (await PrincessModel.findById(product) || await RobotModel.findById(product));
+
+//     if (productInfor) {
+//         var price = productInfor.price;
+//         var image = productInfor.image;
+//         var id = productInfor._id;
+//         total += price * quantity;
+//         productOrder.push({
+//             name: productInfor.name,
+//             price: price,
+//             image: image,
+//             id: id,
+//             quantity: quantity
+
+//         });
+//     }
+// }
+//         total=total.toFixed(2);
+//         res.render('cart/index',{
+//             productOrder:productOrder,
+//             total:total,
+//         })
+//         console.log(productOrder,total);
+//     }catch(err){
+//         console.error(err);
+//     }
+// })
+router.get("/", async (req, res) => {
+  var userId = req.session.userId;
+  let total = 0.0;
+
+  // Kiểm tra xem người dùng đang đăng nhập có là admin hay không
+  if (req.session.role) {
+    try {
+      // Lấy thông tin của tất cả tài khoản đã thêm sản phẩm vào giỏ hàng
+      const allCarts = await CartModel.find();
+
+      // Truy xuất thông tin sản phẩm từ các giỏ hàng
+      const productCart = [];
+
+      for (const cart of allCarts) {
+        const customer = await UserModel.findById(cart.userId);
+        const products = cart.items;
+
+        for (const product of products) {
+          const productInfo =
+            (await PrincessModel.findById(product.product)) ||
+            (await RobotModel.findById(product.product));
+
+          if (productInfo) {
+            const price = productInfo.price;
+            const image = productInfo.image;
+            const id = productInfo._id;
+            const quantity = product.quantity;
+            total += price * quantity;
+            productCart.push({
+              name: productInfo.name,
+              price: price,
+              image: image,
+              id: id,
+              quantity: quantity,
+              total:total
+              // customer: customer
+              // ? ${customer.username}
+              //   : "Unknown"
+            });
+          }
+        }
+      }
+      total = total.toFixed(2);
+      res.render("cart/admin", { productCart: productCart,total: total });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Lỗi khi lấy thông tin giỏ hàng" });
+    }
+  } else {
+        try{
+        var cart=await CartModel.findOne({userId: userId});
         if(!cart){
             return res.status(404).json({message:"Cart not found"})
         }
-        
-        // var items=cart.items;
-        // var ID=items.map((product)=>product.product);
-        // var productOrder=[];
-        // var total=0.0;
-
-        // //Tao mot doi tuong luu so luong tung san pham
-        // // var quantities={};
-        // for(var i=0; i<ID.length; i++){
-        //     var id=ID[i];
-        //     var productInfor=(await PrincessModel.findById(id)|| await RobotModel.findById(id));
-        //     if(productInfor){
-        //         var price=productInfor.price;
-        //         var image=productInfor.image;
-        //         var id=productInfor._id;
-        //         var quantity=ID[i].quantity;
-        //         total+=price*quantity;
-        //         productOrder.push({
-        //             name:productInfor.name,
-        //             price:price,
-        //             image:image,
-        //             id:id,
-        //             quantity:quantity
-        //         })
-        //     }
-        // }
         var items = cart.items;
 var productOrder = [];
-var total = 0.0;
+let total = 0.0;
 
 for (var i = 0; i < items.length; i++) {
     var product = items[i].product;
@@ -61,6 +125,7 @@ for (var i = 0; i < items.length; i++) {
             image: image,
             id: id,
             quantity: quantity
+
         });
     }
 }
@@ -73,7 +138,42 @@ for (var i = 0; i < items.length; i++) {
     }catch(err){
         console.error(err);
     }
-})
+  }
+});
+
+// router.get('/admin',async(req,res)=>{
+
+//   // try {
+//   //   const allCarts = await CartModel.find();
+//   //   const allProducts = [];
+    
+//   //   for (const cart of allCarts) {
+//   //     for (const item of cart.items) {
+//   //       const product = item.product;
+//   //       const quantity = item.quantity;
+        
+//   //       const productInfo = await (
+//   //         PrincessModel.findById(product) || RobotModel.findById(product)
+//   //       );
+        
+//   //       if (productInfo) {
+//   //         allProducts.push({
+//   //           user: cart.userId, // You might want to store user information in the cart
+//   //           productName: productInfo.name,
+//   //           price: productInfo.price,
+//   //           image: productInfo.image,
+//   //           quantity: quantity,
+//   //         });
+//   //       }
+//   //     }
+//   //   }
+
+//   //   res.render('cart/admin', { allProducts: allProducts });
+//   // } catch (err) {
+//   //   console.error(err);
+//   //   // Handle errors
+//   // }
+// });
 
 router.post("/", async (req, res) => {
     var products = req.body.id;
